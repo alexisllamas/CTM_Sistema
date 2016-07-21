049b1b69219db64c31a1a635190a417438f82a4c
Alexis Navarro <alexisnavarro23@gmail.com>
2016-07-20 22:03:00 -0500

    Hola

--- Ctm_Col/CredencialForm.Designer.cs
+++ Ctm_Col/CredencialForm.Designer.cs
@@ -28,19 +28,143 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtApMaterno = new Ctm_Col.MaterialTextBox();
+            this.txtApPaterno = new Ctm_Col.MaterialTextBox();
+            this.txtNombre = new Ctm_Col.MaterialTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dtpFecha = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.materialFlatButton1 = new MaterialSkin.Controls.MaterialFlatButton();
+            this.materialFlatButton2 = new MaterialSkin.Controls.MaterialFlatButton();
             this.SuspendLayout();
+            // 
+            // txtApMaterno
+            // 
+            this.txtApMaterno.Enabled = false;
+            this.txtApMaterno.Location = new System.Drawing.Point(234, 86);
+            this.txtApMaterno.Name = "txtApMaterno";
+            this.txtApMaterno.Placeholder = "Apellido Materno";
+            this.txtApMaterno.Size = new System.Drawing.Size(126, 48);
+            this.txtApMaterno.TabIndex = 68;
+            // 
+            // txtApPaterno
+            // 
+            this.txtApPaterno.Enabled = false;
+            this.txtApPaterno.Location = new System.Drawing.Point(114, 86);
+            this.txtApPaterno.Name = "txtApPaterno";
+            this.txtApPaterno.Placeholder = "Apellido Paterno";
+            this.txtApPaterno.Size = new System.Drawing.Size(114, 48);
+            this.txtApPaterno.TabIndex = 67;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Enabled = false;
+            this.txtNombre.Location = new System.Drawing.Point(23, 86);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Placeholder = "Nombre(s)";
+            this.txtNombre.Size = new System.Drawing.Size(85, 48);
+            this.txtNombre.TabIndex = 66;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Enabled = false;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 167);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 12);
+            this.label4.TabIndex = 70;
+            this.label4.Text = "Fecha de expedición";
+            // 
+            // dtpFecha
+            // 
+            this.dtpFecha.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFecha.Location = new System.Drawing.Point(14, 182);
+            this.dtpFecha.Name = "dtpFecha";
+            this.dtpFecha.Size = new System.Drawing.Size(147, 20);
+            this.dtpFecha.TabIndex = 69;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Enabled = false;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(198, 167);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 12);
+            this.label1.TabIndex = 72;
+            this.label1.Text = "Fecha de vencimiento";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(200, 182);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(147, 20);
+            this.dateTimePicker1.TabIndex = 71;
+            // 
+            // materialFlatButton1
+            // 
+            this.materialFlatButton1.AutoSize = true;
+            this.materialFlatButton1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialFlatButton1.Depth = 0;
+            this.materialFlatButton1.Location = new System.Drawing.Point(23, 227);
+            this.materialFlatButton1.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialFlatButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialFlatButton1.Name = "materialFlatButton1";
+            this.materialFlatButton1.Primary = false;
+            this.materialFlatButton1.Size = new System.Drawing.Size(74, 36);
+            this.materialFlatButton1.TabIndex = 73;
+            this.materialFlatButton1.Text = "Imprimir";
+            this.materialFlatButton1.UseVisualStyleBackColor = true;
+            // 
+            // materialFlatButton2
+            // 
+            this.materialFlatButton2.AutoSize = true;
+            this.materialFlatButton2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialFlatButton2.Depth = 0;
+            this.materialFlatButton2.Location = new System.Drawing.Point(234, 227);
+            this.materialFlatButton2.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialFlatButton2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialFlatButton2.Name = "materialFlatButton2";
+            this.materialFlatButton2.Primary = false;
+            this.materialFlatButton2.Size = new System.Drawing.Size(74, 36);
+            this.materialFlatButton2.TabIndex = 74;
+            this.materialFlatButton2.Text = "Renovar";
+            this.materialFlatButton2.UseVisualStyleBackColor = true;
             // 
             // CredencialForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(300, 300);
+            this.ClientSize = new System.Drawing.Size(388, 283);
+            this.Controls.Add(this.materialFlatButton2);
+            this.Controls.Add(this.materialFlatButton1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dtpFecha);
+            this.Controls.Add(this.txtApMaterno);
+            this.Controls.Add(this.txtApPaterno);
+            this.Controls.Add(this.txtNombre);
             this.Name = "CredencialForm";
             this.Text = "Credencial";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private MaterialTextBox txtApMaterno;
+        private MaterialTextBox txtApPaterno;
+        private MaterialTextBox txtNombre;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dtpFecha;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton1;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton2;
     }
 }
